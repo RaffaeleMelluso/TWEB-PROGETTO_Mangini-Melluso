f1d3ce3f5ca58439f7a24bb19d60fe8f66f93e06
Dario Mangini <manginidario24@gmail.com>
2024-12-27 17:36:58 +0100

    aggiunte pagine per latest e top, modifiche a homepage, routes e i relativi scripts

--- solution/Client/routes/index.js
+++ solution/Client/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+/* GET Ultime Uscite page. */
+router.get('/latest', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/latest.html'));
+});
+
+/* GET Top 10 page. */
+router.get('/top', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/top.html'));
+});
+
+/* GET Oscar page. */
+router.get('/oscar', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/oscar.html'));
 });
 
 module.exports = router;
