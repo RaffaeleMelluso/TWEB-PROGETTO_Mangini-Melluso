1d0d2f275f33a26457e7a6eaaba2347173b05d5c
Raffaele Melluso <raffaelemelluso01@gmail.com>
2025-01-10 15:47:23 +0100

    aggiunto server springboot

---
+++ solution/ServerStaticoSpring/src/test/java/com/example/serverstaticospring/ServerStaticoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverstaticospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStaticoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
